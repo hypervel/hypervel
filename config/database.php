7f518ef2ae22250b2b52f73f374ad671af998f1c
Raj Siva-Rajah <raj@zapzap.cloud>
2025-07-08 09:42:00 +0000

    Add pgsql to database config

--- config/database.php
+++ config/database.php
@@ -32,7 +32,25 @@ return [
             'prefix' => env('DB_PREFIX', ''),
             'pool' => [
                 'min_connections' => 1,
-                'max_connections' => 10,
+                'max_connections' => env('DB_MAX_CONNECTIONS', 10),
+                'connect_timeout' => 10.0,
+                'wait_timeout' => 3.0,
+                'heartbeat' => -1,
+                'max_idle_time' => (float) env('DB_MAX_IDLE_TIME', 60),
+            ],
+        ],
+
+        'pgsql' => [
+            'driver' => env('DB_DRIVER', 'pgsql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'hypervel'),
+            'port' => env('DB_PORT', 5432),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'pool' => [
+                'min_connections' => 1,
+                'max_connections' => env('DB_MAX_CONNECTIONS', 10),
                 'connect_timeout' => 10.0,
                 'wait_timeout' => 3.0,
                 'heartbeat' => -1,
