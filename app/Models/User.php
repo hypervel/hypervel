c90b25c5b450f2f803cac8dce6d19d2b2682016a
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-06 11:03:00 +0000

    Update User model for 0.4
    Add Notifiable trait and $hidden property for password
    and remember_token serialization.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ namespace App\Models;
 
 use Hypervel\Database\Eloquent\Factories\HasFactory;
 use Hypervel\Foundation\Auth\User as Authenticatable;
+use Hypervel\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -17,7 +19,27 @@ class User extends Authenticatable
     protected array $fillable = [
         'name',
         'email',
-        'email_verified_at',
         'password',
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     */
+    protected array $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
