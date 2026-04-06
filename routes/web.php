b2ae2a0e0ba8a3674eaf60581a61efe73db2b19e
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-06 11:03:53 +0000

    Add comment block to web routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 use Hypervel\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
