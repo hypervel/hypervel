434b6f4135f8dcee6a41fc19088c7df48304c984
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-06 11:03:46 +0000

    Simplify console routes comment block

--- routes/console.php
+++ routes/console.php
@@ -10,9 +10,7 @@ use Hypervel\Support\Facades\Schedule;
 | Console Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
+| Define your closure-based console commands here.
 |
 */
 
